34cab7e39e534f849881e6cce717da7c970e67b8
goosebeomsu <captain9697@naver.com>
2024-09-10 00:04:54 +0900

    -

---
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -0,0 +1,32 @@
+package cleancode.minesweeper.tobe;
+
+import cleancode.minesweeper.tobe.gamelevel.Beginner;
+import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
+import cleancode.minesweeper.tobe.io.InputHandler;
+import cleancode.minesweeper.tobe.io.OutputHandler;
+
+public class GameApplication {
+
+    public static void main(String[] args) {
+        GameLevel gameLevel = new Beginner();
+        InputHandler inputHandler = new ConsoleInputHandler();
+        OutputHandler outputHandler = new ConsoleOutputHandler();
+
+        Minesweeper minesweeper = new Minesweeper(gameLevel, inputHandler, outputHandler);
+        minesweeper.initialize();
+        minesweeper.run();
+    }
+
+    /**
+     * DIP
+     *
+     * DI: 필요한 의존성을 외부에서 주입 - "3"
+     * a 객체가 b 객체를 필요로할때 a가 b를 생성하는게 아니라 주입받고자 할때 제 3 자가 해야함 (ioc 컨테이너)
+     *
+     * IoC: 객체 생명주기를 ioc 컨테이너에서 다룸 (프로그램의 제어권이 개발자가아닌 프레임워크로)
+     *
+     */
+
+}
